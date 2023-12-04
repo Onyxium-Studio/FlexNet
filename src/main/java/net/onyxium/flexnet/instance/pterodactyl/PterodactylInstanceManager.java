d4fe7d21596b12ca9b50ec537624b94d47960f51
Xiaobonor <xiao.bo.nor@gmail.com>
2023-12-04 23:59:15 +0800

    Refactor createInstance for retry logic and allocation fixes
    For 「The selected allocation.default is invalid. (Source: allocation.default)」 bug

--- src/main/java/net/onyxium/flexnet/instance/pterodactyl/PterodactylInstanceManager.java
+++ src/main/java/net/onyxium/flexnet/instance/pterodactyl/PterodactylInstanceManager.java
@@ -55,53 +55,77 @@ public class PterodactylInstanceManager implements InstanceManager {
                     )
                     .findFirst();
 
-            if(optAllocation.isEmpty()) {
+            if (optAllocation.isEmpty()) {
                 throw new IllegalStateException("No available allocation found");
             }
             log.info("Allocation {} found, creating server...", optAllocation.get().getAlias());
             log.info("Owner: {} | Egg: {}", owner.getFullName(), egg.getName());
 
-            try {
-                ApplicationServer server = api.createServer()
-                        .setName(template.getNameTemplate())
-                        .setDescription(template.getDescription())
-                        .setAllocations(optAllocation.get())
-                        .setOwner(owner)
-                        .setEgg(egg)
-                        .setCPU(template.getCpuAmount())
-                        .setMemory(template.getMemoryAmount(), DataType.MB)
-                        .setDisk(template.getDiskAmount(), DataType.MB)
-                        .skipScripts(template.isSkipInitScript())
-                        .execute();
-
-                watcher.createTask(
-                        server.getIdentifier(),
-                        clientServer -> {},
-                        clientServer -> {
-                            log.info("Server {} installing: {} suspended: {}", clientServer.getName(), clientServer.isInstalling(), clientServer.isSuspended());
-                            if(clientServer.isInstalling() || clientServer.isSuspended()) return false;
-                            Utilization utilization = clientServer.retrieveUtilization().execute();
-                            log.info("Server {} state = {}", clientServer.getName(), utilization.getState());
-                            if(utilization.getState() == UtilizationState.OFFLINE) {
-                                clientServer.start().execute();
-                            }
-                            return utilization.getState() == UtilizationState.RUNNING;
-                        },
-                        clientServer -> {
-                            resultConsumer.accept(
-                                    InstanceCreationResult.builder()
-                                            .instanceId(server.getIdentifier())
-                                            .instanceName(server.getName())
-                                            .address(new InetSocketAddress(optAllocation.get().getIP(), optAllocation.get().getPortInt()))
-                                            .success(true)
-                                            .build()
-                            );
-                        });
-            } catch (Exception e) {
-                log.error("Failed to create server", e);
-                throw new RuntimeException(e);
+            int attempt = 0;
+            while (attempt < 3) {
+                try {
+                    ApplicationServer server = createServer(template, optAllocation, owner, egg);
+                    executeWatcher(server, resultConsumer, optAllocation);
+                    log.info("Server {} created successfully", server.getName());
+                    return;
+                } catch (Exception e) {
+                    log.error("Attempt {} - Failed to create server: {}", attempt + 1, e.getMessage());
+                    if (attempt == 2) {
+                        log.error("All attempts to create the server have failed.");
+                    }
+                }
+                attempt++;
             }
         }).join();
+    }
+
+    private ApplicationServer createServer(InstanceTemplate template, Optional<ApplicationAllocation> optAllocation,
+                                           ApplicationUser owner, ApplicationEgg egg) throws Exception {
+        if (optAllocation.isEmpty()) {
+            throw new IllegalStateException("Allocation is not present");
+        }
+
+        return api.createServer()
+                .setName(template.getNameTemplate())
+                .setDescription(template.getDescription())
+                .setAllocations(optAllocation.get())
+                .setOwner(owner)
+                .setEgg(egg)
+                .setCPU(template.getCpuAmount())
+                .setMemory(template.getMemoryAmount(), DataType.MB)
+                .setDisk(template.getDiskAmount(), DataType.MB)
+                .skipScripts(template.isSkipInitScript())
+                .execute();
+    }
+
+    private void executeWatcher(ApplicationServer server, Consumer<InstanceCreationResult> resultConsumer,
+                                Optional<ApplicationAllocation> optAllocation) {
+        if (optAllocation.isEmpty()) {
+            throw new IllegalStateException("Allocation is not present");
+        }
+
+        watcher.createTask(
+                server.getIdentifier(),
+                clientServer -> {},
+                clientServer -> {
+                    log.info("Server {} installing: {} suspended: {}", clientServer.getName(), clientServer.isInstalling(), clientServer.isSuspended());
+                    if (clientServer.isInstalling() || clientServer.isSuspended()) return false;
+                    Utilization utilization = clientServer.retrieveUtilization().execute();
+                    log.info("Server {} state = {}", clientServer.getName(), utilization.getState());
+                    if (utilization.getState() == UtilizationState.OFFLINE) {
+                        clientServer.start().execute();
+                    }
+                    return utilization.getState() == UtilizationState.RUNNING;
+                },
+                clientServer -> resultConsumer.accept(
+                        InstanceCreationResult.builder()
+                                .instanceId(server.getIdentifier())
+                                .instanceName(server.getName())
+                                .address(new InetSocketAddress(optAllocation.get().getIP(), optAllocation.get().getPortInt()))
+                                .success(true)
+                                .build()
+                )
+        );
     }
 
     @Override
